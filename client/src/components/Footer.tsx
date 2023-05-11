e2db4c128254991b7a1510b062bcd41bf5983d2e
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-05-11 04:14:59 +0300

    Design and build footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,9 +2,43 @@ import React from "react";
 import styled from "styled-components";
 
 const Footer: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <FooterContainer>
+      <LeftContainer>
+        <Logo>Furnish</Logo>
+      </LeftContainer>
+      <RightContainer>
+        
+      </RightContainer>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
 
-const Container = styled.div``;
+const FooterContainer = styled.div`
+  border-top: 1.5px solid #ece4de;
+  margin: 0 100px;
+  margin-bottom: 100px;
+  height: 300px;
+  color: #fff;
+  background-color: #518581;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+`;
+
+const RightContainer = styled.div``;
+
+const Logo = styled.a`
+  font-size: 2rem;
+  font-weight: 700;
+  text-decoration: none;
+  height: 18px;
+`;
