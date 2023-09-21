c372e06bb9fd86bbf6163e968d26e74c295c7f20
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-09-21 15:39:51 +0300

    Make improvement to the navbar

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,36 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
+import { useState, useRef, } from "react";
+import { NavbarModal } from "..";
+
 const Navbar = () => {
+  const [showProfileModal, setShowProfileModal] = useState<boolean>(false);
+  const modalRef = useRef(null);
+
+  const handleShowProfileModal = () => {
+    setShowProfileModal((prev) => !prev);
+  };
+
+  // Close the modal when a click occurs outside of it
+  // const handleDocumentClick = (e: MouseEvent) => {
+  //   if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+  //     setShowProfileModal(false);
+  //   }
+  // };
+ 
+  // useEffect(() => {
+  //   if (showProfileModal) {
+  //     window.addEventListener("click", handleDocumentClick);
+  //   } else {
+  //     window.removeEventListener("click", handleDocumentClick);
+  //   }
+
+  //   return () => {
+  //     window.removeEventListener("click", handleDocumentClick);
+  //   };
+  // }, [showProfileModal]);
+
   return (
     <div className="navbar outer-padding flex-row justify-space_between align-center">
       <NavLink to="/">
@@ -13,13 +43,26 @@ const Navbar = () => {
         <NavLink to="/products" className="nav-link">
           Products
         </NavLink>
-        <NavLink to="/cart" className="nav-link">
-          Cart
-        </NavLink>
-        <NavLink to="/profile" className="nav-link">
+        <div
+          className="nav-link flex-row align-center"
+          onClick={handleShowProfileModal}
+        >
           Profile
+          <AiOutlineUser className="react-icon" />
+        </div>
+        <NavLink to="/cart" className="nav-link flex-row align-center">
+          Cart
+          <AiOutlineShoppingCart className="react-icon" />
         </NavLink>
       </nav>
+      {showProfileModal && (
+        <div
+          className={`profile-modal ${showProfileModal ? "show" : ""}`}
+          ref={modalRef}
+        >
+          <NavbarModal />
+        </div>
+      )}
     </div>
   );
 };
