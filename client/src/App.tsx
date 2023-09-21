7e9693d2e3266236c31ce01d2d29f5d4c734e694
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-09-21 15:44:42 +0300

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,21 +6,24 @@ import {
   SingleProductPage,
   CheckoutPage,
   CartPage,
+  Login,
 } from "./Pages";
 import { Footer, Navbar } from "./Components";
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductListPage />} />
           <Route path="/products/:id" element={<SingleProductPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" />
         </Routes>
       </main>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
--- client/src/Pages/index.ts
+++ client/src/Pages/index.ts
@@ -4,4 +4,6 @@ import ProductListPage from "./ProductListPage/ProductListPage";
 import SingleProductPage from "./SingleProductPage/SingleProductPage";
 import CheckoutPage from "./CheckoutPage/CheckoutPage";
 import CartPage from "./CartPage/CartPage";
-export { HomePage, OrderPage, ProductListPage, SingleProductPage, CheckoutPage, CartPage }
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+export { HomePage, OrderPage, ProductListPage, SingleProductPage, CheckoutPage, CartPage, Login, Register }
